0a6291600f0769bd436f78eddffb8ba74641ff8c
Rishi Patel <rpatel@cern.ch>
2015-01-13 15:50:35 +0100

    Cleaner Version (compiled Macro)

---
+++ MakeDataCards.C
@@ -0,0 +1,267 @@
+//
+//  MakeDataCards.C
+//  
+//
+//  Created by Rishi Patel on 1/13/15.
+//
+//
+
+#include <TH3F.h>
+#include <TChain.h>
+#include <TFile.h>
+#include <iostream>
+#include <stdio.h>
+#include <TString.h>
+void MakeDataCards(int mGlu, int MLSP, TString Options="",  bool SingleBin=false, int Limitbin=0){
+    //DEFINE Options FOR DataCards
+    //Sum Bkg Yields: "SUMBKG"
+    //Include Systematics: "SYS"
+    // can AND Options with '&&'
+    
+//define binning (for now rectangular):
+
+    
+    const int nBinsjets = 3 ;
+    const int nBinsBjets = 4 ;
+    const int nBinsMHT = 3 ;
+    const int nBinsHT = 3 ;
+
+    //array of the signal and background yields:
+    TH3F*sig4t_[4];
+    TH3F*qcd_[4];
+    TH3F*tt_[4];
+    TH3F*WJ_[4];
+    TH3F*ZI_[4];
+    
+    TFile*f0=new TFile(TString::Format("Bins4D_mGlu%d_MLSP%d.root", mGlu,MLSP).Data());
+    for(int b=0; b<nBinsBjets; ++b){
+        sig4t_[b]=(TH3F*)f0->Get(TString::Format("sig4t__b%d",b).Data());
+        qcd_[b]=(TH3F*)f0->Get(TString::Format("qcd__b%d",b).Data());
+        tt_[b]=(TH3F*)f0->Get(TString::Format("tt__b%d",b).Data());
+        WJ_[b]=(TH3F*)f0->Get(TString::Format("WJ__b%d",b).Data());
+        ZI_[b]=(TH3F*)f0->Get(TString::Format("ZI__b%d",b).Data());
+    }
+    const int totalbins=nBinsjets*nBinsHT*nBinsMHT*nBinsBjets;
+
+    float sig[totalbins];
+    float qcd[totalbins];
+    float zi[totalbins];
+    float wj[totalbins];
+    float ttbar[totalbins];
+    float obs[totalbins];
+    float nuis[totalbins];//this would be the systematics
+    
+    int ibin=0;//bin counter
+    float mu=0; //change this to add signal to the observed data for tests (DEFAULT obs set to SM)
+    for(int nb=0; nb<=3; nb++){
+        for ( int hti=1; hti<=3; hti++ ) {
+            for ( int mhti=1; mhti<=3; mhti++ ) {
+                for ( int ij=1; ij<=3; ij++ ) {
+                    
+                    {
+                        // std::cout<<sig4t_[nb]->GetBinContent( ij, mhti, hti )<<std::endl;
+                        sig[ibin]=sig4t_[nb]->GetBinContent( ij, mhti, hti );
+                        qcd[ibin]=qcd_[nb]->GetBinContent( ij, mhti, hti );
+                        zi[ibin]=ZI_[nb]->GetBinContent( ij, mhti, hti );
+                        wj[ibin]=WJ_[nb]->GetBinContent( ij, mhti, hti );
+                        ttbar[ibin]=tt_[nb]->GetBinContent( ij, mhti, hti );
+                        //for now observed data is set to total bkg yield
+                        obs[ibin]=qcd[ibin]+ zi[ibin]+wj[ibin]+ttbar[ibin] + (mu*sig[ibin]);
+                        if( qcd[ibin]+zi[ibin]+wj[ibin]+ttbar[ibin]<0.00000001)std::cout<<"ERROR:Bkg 0 for this bin"<<std::endl;
+                        ++ibin;
+                    }
+                }
+            }
+        }
+    }
+    return;
+
+    //define different datacard modes
+    if(SingleBin){
+    TString str=(TString::Format("datfiles/SensStudyBin%d_M%d_m%d_DEFAULT.dat",Limitbin, mGlu, MLSP)).Data();
+   // const char outputfilename[100]=str.Data();
+    
+    FILE* fp = fopen(  str.Data(), "w" ) ;
+    
+    fprintf(fp, "imax %d number of channels\n", 1);
+        if(Options=="SUMBKG") fprintf(fp, "jmax %d number of backgrounds\n",1);
+        else fprintf(fp, "jmax %d number of backgrounds (QCD, Zinv, WJ, ttbar)\n",4);
+        fprintf(fp, "kmax * nuissance\n");
+        fprintf(fp, "------------\n");
+        fprintf(fp, "bin %d", Limitbin);
+        fprintf(fp, "\n observation %g", obs[Limitbin]);
+        
+        if(Options=="SUMBKG"){
+            fprintf(fp, "\nbin %d %d ", Limitbin, Limitbin);
+            fprintf(fp, "\nprocess sig bkg ");
+            fprintf(fp, "\nprocess %d %d ", 0, 1);
+            fprintf(fp, "\nrate %g %g ", sig[Limitbin], qcd[Limitbin]+ zi[Limitbin]+wj[Limitbin]+ttbar[Limitbin]);
+        }
+        else{
+            fprintf(fp, "\nbin %d %d %d %d", Limitbin, Limitbin,Limitbin, Limitbin);
+            fprintf(fp, "\nsig QCD, Zinv, WJ, ttbar");
+
+            fprintf(fp, "\nprocess %d %d %d %d %d", 0, 1,2,3,4);
+            fprintf(fp, "\nrate %g %g %g %g %g ", sig[Limitbin], qcd[Limitbin], zi[Limitbin],wj[Limitbin],ttbar[Limitbin]);
+        }
+    }
+    
+
+    else{
+        TString str=(TString::Format("datfiles/SensStudy_M%d_m%d_DEFAULT.dat", mGlu, MLSP)).Data();
+      //  const char outputfilename[100]=str.Data();
+        
+        FILE* fp = fopen( str.Data(), "w" ) ;
+        fprintf(fp, "imax %d number of channels\n", ibin+1);
+        if(Options=="SUMBKG") fprintf(fp, "jmax %d number of backgrounds\n",1);
+        else fprintf(fp, "jmax %d number of backgrounds (QCD, Zinv, WJ, ttbar)\n",4);
+        fprintf(fp, "kmax * nuissance\n");
+        fprintf(fp, "------------\n");
+        fprintf(fp, "bin ");
+        for(int i=0; i<ibin; ++i)fprintf(fp, " %d", i);
+        
+        fprintf(fp, "\n observation ");
+        for(int i=0; i<ibin; ++i)fprintf(fp, "%g ",obs[i]);
+        if(Options=="SUMBKG"){
+            fprintf(fp, "\nbin ");
+            for(int i=0; i<totalbins; ++i)fprintf(fp, "%d %d ", i, i);
+            fprintf(fp, "\nprocess ");
+            for(int i=0; i<totalbins; ++i)fprintf(fp, "sig bkg ");
+            fprintf(fp, "\nprocess ");
+            for(int i=0; i<totalbins; ++i)fprintf(fp, " %d %d ", 0, 1);
+            fprintf(fp, "\nrate ");
+            for(int i=0; i<totalbins; ++i)fprintf(fp, " %g %g ",sig[i], qcd[i]+ zi[i]+wj[i]+ttbar[i]);
+
+        }
+        else{
+            fprintf(fp, "\nbin ");
+            for(int i=0; i<totalbins; ++i)fprintf(fp, "%d %d %d %d %d ", i, i, i, i, i);
+            fprintf(fp, "\nprocess ");
+            for(int i=0; i<totalbins; ++i)fprintf(fp, "sig QCD, Zinv, WJ, ttbar ");
+            fprintf(fp, "\nprocess ");
+            for(int i=0; i<totalbins; ++i)fprintf(fp, " %d %d %d %d %d", 0,1,2,3,4);
+            fprintf(fp, "\nrate ");
+            for(int i=0; i<totalbins; ++i)fprintf(fp, " %g %g %g %g %g",sig[i], qcd[i], zi[i],wj[i],ttbar[i]);
+            
+            if(Options=="SYS"){
+                //here you would add the bkg systematics for each bin
+                
+            }
+
+        }
+    }
+}
+
+void MakeInputHiso(int mGlu, int MLSP){  //all input files
+    const int nBinsjets = 3 ;
+    const int nBinsBjets = 4 ;
+    const int nBinsMHT = 3 ;
+    const int nBinsHT = 3 ;
+    
+    float MHTBinRectangular[4]={200, 500., 750.,9999.};
+    float HTBinRectangular[4]={500., 800., 1200.,9999.};//can do 150 to
+    
+    char bcutstringRectangular[nBinsBjets][100] = { "nbjets==0", "nbjets==1","nbjets==2", "nbjets>=3" } ;
+    float NJets[4]={3, 6,8,99};
+
+TChain t1tt("reducedTree");
+t1tt.Add(TString::Format("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.SMS-T1tttt_2J_mGl-%d_mLSP-%d_Tune4C_13TeV-madgraph-tauola_Spring14miniaod-PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2120_v73-skim.root", mGlu, MLSP ));
+
+TChain QCDCH("reducedTree") ;
+QCDCH.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.QCD_Pt-1000to1400_Tune4C_13TeV_pythia8_Spring14miniaod-castor_PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2118_v73-skim.root");
+QCDCH.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.QCD_Pt-10to15_Tune4C_13TeV_pythia8_Spring14miniaod-castor_PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2103_v73-skim.root");              QCDCH.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.QCD_Pt-120to170_Tune4C_13TeV_pythia8_Spring14miniaod-castor_PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2109_v73-skim.root");
+QCDCH.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.QCD_Pt-1400to1800_Tune4C_13TeV_pythia8_Spring14miniaod-castor_PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2101_v73-skim.root");
+QCDCH.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.QCD_Pt-15to30_Tune4C_13TeV_pythia8_Spring14miniaod-castor_PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2104_v73-skim.root");
+QCDCH.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.QCD_Pt-170to300_Tune4C_13TeV_pythia8_Spring14miniaod-castor_PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2098_v73-skim.root");
+QCDCH.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.QCD_Pt-1800to2400_Tune4C_13TeV_pythia8_Spring14miniaod-PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2100_v73-skim.root");
+QCDCH.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.QCD_Pt-2400to3200_Tune4C_13TeV_pythia8_Spring14miniaod-PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2099_v73-skim.root");
+QCDCH.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.QCD_Pt-300to470_Tune4C_13TeV_pythia8_Spring14miniaod-castor_PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2135_v73-skim.root");
+QCDCH.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.QCD_Pt-30to50_Tune4C_13TeV_pythia8_Spring14miniaod-castor_PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2126_v73-skim.root");
+QCDCH.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.QCD_Pt-3200_Tune4C_13TeV_pythia8_Spring14miniaod-PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2136_v73-skim.root");
+QCDCH.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.QCD_Pt-470to600_Tune4C_13TeV_pythia8_Spring14miniaod-castor_PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2137_v73-skim.root");
+QCDCH.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.QCD_Pt-50to80_Tune4C_13TeV_pythia8_Spring14miniaod-castor_PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2127_v73-skim.root");
+QCDCH.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.QCD_Pt-5to10_Tune4C_13TeV_pythia8_Spring14miniaod-castor_PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2125_v73-skim.root");
+QCDCH.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.QCD_Pt-600to800_Tune4C_13TeV_pythia8_Spring14miniaod-castor_PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2138_v73-skim.root");
+QCDCH.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.QCD_Pt-800to1000_Tune4C_13TeV_pythia8_Spring14miniaod-castor_PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2130_v73-skim.root");
+QCDCH.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.QCD_Pt-80to120_Tune4C_13TeV_pythia8_Spring14miniaod-castor_PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2128_v73-skim.root");
+
+TChain WJets("reducedTree");
+WJets.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.WJetsToLNu_HT-100to200_Tune4C_13TeV-madgraph-tauola_Spring14miniaod-PU20bx25_POSTLS170_V5-v2_MINIAODSIM_UCSB2097_v73-skim.root");
+WJets.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.WJetsToLNu_HT-200to400_Tune4C_13TeV-madgraph-tauola_Spring14miniaod-PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2096_v73-skim.root");
+WJets.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.WJetsToLNu_HT-400to600_Tune4C_13TeV-madgraph-tauola_Spring14miniaod-PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2121_v73-skim.root");
+WJets.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.WJetsToLNu_HT-600toInf_Tune4C_13TeV-madgraph-tauola_Spring14miniaod-PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2122_v73-skim.root");
+
+TChain tt("reducedTree");
+
+tt.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.TBarToLeptons_s-channel-CSA14_Tune4C_13TeV-aMCatNLO-tauola_Spring14miniaod-PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2090_v73-skim.root");
+tt.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.TBarToLeptons_t-channel_Tune4C_CSA14_13TeV-aMCatNLO-tauola_Spring14miniaod-PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2092_v73-skim.root");
+tt.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola-Spring14miniaod-PU20bx25_POSTLS170_V5-v2_MINIAODSIM_UCSB2087_v73-skim.root");
+tt.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.TTJets_MSDecaysCKM_central_Tune4C_13TeV-madgraph-tauola_Spring14miniaod-PU_S14_POSTLS170_V6-v1_MINIAODSIM_UCSB2088_v73-skim.root");
+tt.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.TToLeptons_s-channel-CSA14_Tune4C_13TeV-aMCatNLO-tauola_Spring14miniaod-PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2091_v73-skim.root");
+tt.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.TToLeptons_t-channel-CSA14_Tune4C_13TeV-aMCatNLO-tauola_Spring14miniaod-PU20bx25_POSTLS170_V5-v2_MINIAODSIM_UCSB2107_v73-skim.root");
+tt.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.T_tW-channel-DR_Tune4C_13TeV-CSA14-powheg-tauola_Spring14miniaod-PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2093_v73-skim.root");
+tt.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.Tbar_tW-channel-DR_Tune4C_13TeV-CSA14-powheg-tauola_Spring14miniaod-PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2094_v73-skim.root");
+
+TChain Zinv("reducedTree") ;
+Zinv.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.ZJetsToNuNu_HT-100to200_Tune4C_13TeV-madgraph-tauola_Spring14miniaod-PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2106_v73-skim.root");
+Zinv.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.ZJetsToNuNu_HT-200to400_Tune4C_13TeV-madgraph-tauola_Spring14miniaod-PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2108_v73-skim.root");
+Zinv.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.ZJetsToNuNu_HT-400to600_Tune4C_13TeV-madgraph-tauola_Spring14miniaod-PU20bx25_POSTLS170_V5-v1_MINIAODSIM_UCSB2111_v73-skim.root");
+Zinv.Add("reducedTree.JES0_JER0_PFMETTypeI_METunc0_PUunc0_hpt20.ZJetsToNuNu_HT-600toInf_Tune4C_13TeV-madgraph-tauola_Spring14miniaod-PU20bx25_POSTLS170_V5-v2_MINIAODSIM_UCSB2112_v73-skim.root");
+
+const int nJetsCut = 3 ;
+double minLeadJetPt = 50. ;
+double min3rdJetPt = 50. ;
+char commoncuts[10000] ;
+
+sprintf( commoncuts,"HT>500 && njets>=4 && MET>200 && nMuons==0 && nElectrons==0 && minDeltaPhiN_asin>4.0 && jetpt1>%g && jetpt2>%g && jetpt3>%g", minLeadJetPt,min3rdJetPt,min3rdJetPt ); //event selection
+
+char weightstring[10000];
+sprintf(weightstring, "*(weight3*5000)"); //bkg xsec weight
+
+char weightstringSig[10000];
+sprintf(weightstringSig, "*((0.0141903 *5000)/(105679.0))"); //sig xsec added by hand for phys14 it will be in the ntuple
+char arg1[1000] ;
+char cuts[10000];
+//yields stored in a TH3* (one for each btag) so 4D
+TH3F*sig4t_[4];
+TH3F*qcd_[4];
+TH3F*tt_[4];
+TH3F*WJ_[4];
+TH3F*ZI_[4];
+
+//REALLY NEED TO ONLY DO THIS ONCE
+TFile*f0=new TFile(TString::Format("Bins4D_mGlu%d_MLSP%d.root", mGlu,MLSP).Data(), "RECREATE");
+for(int b=0; b<nBinsBjets; ++b){
+    
+    sig4t_[b]=new TH3F(TString::Format("sig4t__b%d", b).Data(), "", nBinsjets, NJets,nBinsMHT,MHTBinRectangular,nBinsHT,HTBinRectangular  );
+    qcd_[b]=new TH3F(TString::Format("qcd__b%d", b).Data(), "", nBinsjets, NJets,nBinsMHT,MHTBinRectangular, nBinsHT,HTBinRectangular );
+    tt_[b]=new TH3F(TString::Format("tt__b%d", b).Data(), "", nBinsjets, NJets,nBinsMHT,MHTBinRectangular,nBinsHT,HTBinRectangular  );
+    WJ_[b]=new TH3F(TString::Format("WJ__b%d", b).Data(), "", nBinsjets, NJets,nBinsMHT,MHTBinRectangular,nBinsHT,HTBinRectangular  );
+    ZI_[b]=new TH3F(TString::Format("ZI__b%d", b).Data(), "", nBinsjets, NJets,nBinsMHT,MHTBinRectangular,nBinsHT,HTBinRectangular  );
+    
+    sprintf( cuts, "(%s && %s )%s", commoncuts,bcutstringRectangular[b],weightstringSig );
+    sprintf( arg1, TString::Format("HT:MHT:njets>>sig4t__b%d",b).Data()) ;
+    
+    t1tt.Draw( arg1, cuts );
+    
+    
+    sprintf( cuts, "(%s && %s )%s", commoncuts,bcutstringRectangular[b],weightstring );
+    
+    
+    
+    sprintf( arg1, TString::Format("HT:MHT:njets>>qcd__b%d",b).Data()) ;
+    QCDCH.Draw( arg1, cuts ) ;
+    sprintf( arg1, TString::Format("HT:MHT:njets>>tt__b%d",b).Data()) ;
+    tt.Draw( arg1, cuts ) ;
+    sprintf( arg1, TString::Format("HT:MHT:njets>>WJ__b%d",b).Data()) ;
+    WJets.Draw( arg1, cuts );
+    sprintf( arg1, TString::Format("HT:MHT:njets>>ZI__b%d",b).Data()) ;
+    Zinv.Draw( arg1, cuts ) ;
+    
+    sig4t_[b]->Write();
+    qcd_[b]->Write();
+    WJ_[b]->Write();
+    tt_[b]->Write();
+    ZI_[b]->Write();
+    }
+}
