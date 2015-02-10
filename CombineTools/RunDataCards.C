0067da2987a51e7e78c53d79d73266efcd14aa49
Rishi Patel <rpatel@cern.ch>
2015-02-10 16:00:23 +0100

    Clean version

--- CombineTools/RunDataCards.C
+++ CombineTools/RunDataCards.C
@@ -1,19 +1,13 @@
 void RunDataCards(){
-gROOT->ProcessLine(".L MakeDataCards_C.so");
-//MakeDataCards(1500,100,i, "T1tttt");
-//MakeDataCards(1200,800,i, "T1tttt");
-for(int i=1; i<8; ++i){
-MakeDataCards(1500,100,i, "T1tttt");
-MakeDataCards(1200,800,i, "T1tttt");
-MakeDataCards(1500,100,i, "T1bbbb");
-MakeDataCards(1000,900,i, "T1bbbb");
-MakeDataCards(1400,100,i, "T1qqqq");
-MakeDataCards(1000,800,i, "T1qqqq");
-//for(int j=0; j<72; ++j)MakeDataCards(1500,100,5, "T1tttt",true, j);
-//for(int j=0; j<72; ++j)MakeDataCards(1200,800,5, "T1tttt",true, j);;
-//for(int j=0; j<72; ++j)MakeDataCards(1500,100,i, "T1bbbb",true, j);;
-//for(int j=0; j<72; ++j)MakeDataCards(1000,900,i, "T1bbbb",true, j);;
-//for(int j=0; j<72; ++j)MakeDataCards(1400,100,i, "T1qqqq",true, j);;
-//for(int j=0; j<72; ++j)MakeDataCards(1000,800,i, "T1qqqq",true, j);;
-}
+gROOT->ProcessLine(".L MakeCombineDataCards_C.so");
+    for(int i=0; i<72; ++i){
+        MakeCombineDataCards(1500, 100, 4, "T1tttt", true, i);
+        MakeCombineDataCards(1200, 800, 4, "T1tttt", true, i);
+        MakeCombineDataCards(1500, 100, 4, "T1bbbb", true, i);
+        MakeCombineDataCards(1000, 800, 4, "T1bbbb", true, i);
+        MakeCombineDataCards(1400, 100, 4, "T1qqqq", true, i);
+        MakeCombineDataCards(1000, 900, 4, "T1qqqq", true, i);
+
+	}
+   
 }
